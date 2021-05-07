addf694225fe6619390d97bd10df54150ec2e4f0
Aleksey Chistyakov <chskela@gmail.com>
2021-05-07 14:00:39 +0300

    add usePosition, update useFetchData

--- hooks/useFetchData.js
+++ hooks/useFetchData.js
@@ -1,9 +1,20 @@
-import config from '../configs/config';
-import { useRequest } from 'ahooks';
+import useSWR from 'swr';
 
-export default function useFetchData(city) {
-  const { API_KEY, URI } = config;
-  const uri = `${URI}q=${city}&units=metric&appid=${API_KEY}`;
-  const { data, error } = useRequest(uri);
-  return [data, error];
+import config from '../configs/config';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+export default function useFetchData(position, city = null) {
+  const {API_KEY, URI} = config;
+  const {latitude, longitude} = position;
+  const query = city ? `q=${city}` : `lat=${latitude}&lon=${longitude}`;
+  const uri = `${URI}${query}&units=metric&appid=${API_KEY}`;
+
+  const {data, error, isValidating} = useSWR(uri, fetcher);
+  console.log(isValidating);
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 }
---
+++ hooks/usePosition.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+
+export default function usePosition() {
+  const [position, setPosition] = useState({latitude: 0, longitude: 0});
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setPosition({latitude, longitude});
+      },
+      error => setError(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+  }, []);
+
+  return {position, error};
+}
